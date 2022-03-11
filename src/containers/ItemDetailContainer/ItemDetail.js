5beaf870e273bf890f291bf7415a285a644a06ca
AgustinErcoli <ercoliagustin@gmail.com>
2022-03-10 23:54:57 -0300

    Details

--- src/containers/ItemDetailContainer/ItemDetail.js
+++ src/containers/ItemDetailContainer/ItemDetail.js
@@ -1,37 +1,14 @@
-import { useState, useEffect } from "react";
-import { getFetch } from "../../helpers/getFetch";
-import Detail from "./Detail";
 import './ItemDetail.css';
 
+const ItemDetail = ({ item }) => {
 
-function ItemDetail() {
 
-    const [combos, setCombos] = useState([])
-    const [loading, setLoading] = useState(true)
-
-useEffect(() => {
-        getFetch
-        .then((respuesta)=> {
-            return respuesta
-        })
-        .then((resp) => setCombos(resp.find(r => r.id === 1)))
-        .catch(err => console.log(err))
-        .finally(()=> setLoading(false))
-    }, [])
-
-  return (
-      <div>
-            <>
-                {   loading ? <h1>Cargando...</h1>
-                            :
-                            combos.map((prod) => 
-                                <Detail details={ prod } />
-                            )
-                }
-            </>
-
-      </div>       
-  )
-}
+      return (
+        <section>
+        <p>{item.title}</p>
+        <img src={item.img} alt={item.title}></img>
+        </section>
+      )
+    }
 
 export default ItemDetail
--- src/containers/ItemDetailContainer/ItemDetailContainer.js
+++ src/containers/ItemDetailContainer/ItemDetailContainer.js
@@ -1,13 +1,36 @@
-
+import {useState, useEffect} from 'react';
 import ItemDetail from './ItemDetail';
 import './ItemDetailContainer.css';
+import { getFetch } from '../../helpers/getFetch';
 
-function ItemDetailContainer() {
 
+const ItemDetailContainer = () => {
+    const [loading, setLoading] = useState(false);
+    const [prod, setProd] = useState();
+  
+    
+    useEffect(() => {
+      setLoading(true)
+      getFetch()
+        .then(res => setProd(res.find(product => product.id === 1)))
+        .finally(
+          setLoading(false)
+        )
+      }, [])
+  
     return (
-        <div> 
-            <ItemDetail />
-        </div>
-    )}
-
-export default ItemDetailContainer
+      <>
+        {
+          loading
+            ?
+            <div>
+              <h1>Cargando...</h1>
+            </div>
+            :
+            <ItemDetail item={prod} />
+        }
+      </>
+    )
+  }
+  
+  export default ItemDetailContainer
